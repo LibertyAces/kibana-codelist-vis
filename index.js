b279aca49ac2ba40fff93464e6920f8dfd4f7062
awichera <46750967+awichera@users.noreply.github.com>
2019-03-28 16:08:58 +0100

    query ES to include all lookups. Not just 10. (#2)

--- index.js
+++ index.js
@@ -27,15 +27,30 @@ export default function (kibana) {
 					method: 'GET',
 					handler(req, reply) {
 						// Getting the ES data cluster
-						var data = server.plugins.elasticsearch.getCluster('data');
+						var data = server.plugins.elasticsearch.getCluster('data');	
+						
 						// https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/api-reference.html
-						data.callWithRequest(req, 'search', {
-							index: '.x-lff-lookup',
-						}).then(function (response) {
-							reply(response);
-						}, function (response) {
-							reply(response);
-						});
+						data.callWithRequest (
+							req,
+							'count',
+							{index:'.x-lff-lookup'}
+						).then (
+							response => {
+								
+								data.callWithRequest(
+									req, 
+									'search', 
+									{
+										index: '.x-lff-lookup', 
+										size: response.count
+									}
+								).then(function (response) {
+									reply(response);
+								}, function (response) {
+									reply(response);
+								});
+							}
+						)
 					}
 			});
 		}
